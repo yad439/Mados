c16d392e7f98856df5a221b64928638b2e90a60c
Vedblade <18503283+yad439@users.noreply.github.com>
2023-06-17 13:11:44 +0000

    Add empty order list handling

--- src/instancegenerator.jl
+++ src/instancegenerator.jl
@@ -8,17 +8,25 @@ function generateorders(planningperiod::Integer, λ::AbstractFloat)
     [(d, n) for (d, n) ∈ enumerate(rand(dist, planningperiod)) if n ≠ 0]
 end
 
-function generateitem(unitcount::Int, planningperiod::Int, λ::Float64, ρ::Float64, c₀::Float64, ρc::Float64, centraltime_dist, localtimes_dist)
-    ν = λ / ρ * rand(Float64)^((1 - ρ) / ρ)
+function generateitem(unitcount::Int, planningperiod::Int, λ::Float64, ρ::Float64, λₘ::Float64,
+    c₀::Float64, ρc::Float64, cₘ::Float64, centraltime_dist, localtimes_dist)
+    ν = max(λ / ρ * rand(Float64)^((1 - ρ) / ρ), λₘ)
     orders = Iterators.filter(!isempty, (generateorders(planningperiod, ν * 2rand(Float64)) for _ = 1:unitcount)) |> collect
+    if isempty(orders)
+        @warn "No orders generated for item"
+        return nothing
+    end
     central_leadtime = rand(centraltime_dist)
     local_leadtimes = rand(localtimes_dist, length(orders))
-    cost = c₀ / ρc * rand(Float64)^((1 - ρc) / ρc)
+    cost = max(c₀ / ρc * rand(Float64)^((1 - ρc) / ρc), cₘ)
 
     Item(cost, central_leadtime, local_leadtimes, orders)
 end
 
-function generateinstance(itemcount::Int, unitcount::Int, planningperiod::Int, λ::Float64, ρ::Float64, c₀::Float64, ρc::Float64, centraltime_dist, localtimes_dist)
-    items = [generateitem(unitcount, planningperiod, λ, ρ, c₀, ρc, centraltime_dist, localtimes_dist) for _ = 1:itemcount]
+function generateinstance(itemcount::Int, unitcount::Int, planningperiod::Int, demand_mean::Float64,
+    demand_skewness::Float64, demand_min::Float64, cost_mean::Float64, cost_skewness::Float64, cost_min::Float64,
+    centraltime_dist, localtimes_dist)
+    items = Iterators.filter(!isnothing, generateitem(unitcount, planningperiod, demand_mean, demand_skewness,
+        demand_min, cost_mean, cost_skewness, cost_min, centraltime_dist, localtimes_dist) for _ = 1:itemcount) |> collect
     Instance(items, planningperiod)
 end
--- test/instancegenerator.jl
+++ test/instancegenerator.jl
@@ -9,26 +9,48 @@ using Mados: generateinstance, generateitem, generateorders, validate
 end
 
 @testset "generateitem" begin
-    result = @inferred generateitem(10, 11, 1.0, 0.5, 20.0, 0.5, 10:20, 1:9)
+    @testset "Normal generation" begin
+        result = @inferred Nothing generateitem(10, 11, 1.0, 0.5, 0.0, 20.0, 0.5, 0.0, 10:20, 1:9)
 
-    @test result.cost > 0.0
-    @test 10 ≤ result.central_leadtime ≤ 20
-    @test all(1 ≤ l ≤ 9 for l ∈ result.local_leadtimes)
-    @test 0 < length(result.orders) ≤ 10
-    @test all(n > 0 for unitorders ∈ result.orders for (d, n) ∈ unitorders)
-    @test all(d ∈ 1:11 for unitorders ∈ result.orders for (d, n) ∈ unitorders)
+        @test result.cost > 0.0
+        @test 10 ≤ result.central_leadtime ≤ 20
+        @test all(1 ≤ l ≤ 9 for l ∈ result.local_leadtimes)
+        @test 0 < length(result.orders) ≤ 10
+        @test all(n > 0 for unitorders ∈ result.orders for (d, n) ∈ unitorders)
+        @test all(d ∈ 1:11 for unitorders ∈ result.orders for (d, n) ∈ unitorders)
+    end
+    @testset "No orders" begin
+        result = @test_warn "No orders" (@inferred Nothing generateitem(10, 11, 0.0, 0.5, 0.0, 20.0, 0.5, 0.0, 10:20, 1:9))
+
+        @test result ≡ nothing
+    end
 end
 
 @testset "generateinstance" begin
-    result = @inferred generateinstance(10, 10, 11, 1.0, 0.5, 20.0, 0.5, 10:20, 1:9)
+    @testset "Normal generation" begin
+        result = @inferred generateinstance(10, 10, 11, 1.0, 0.5, 0.0, 20.0, 0.5, 0.0, 10:20, 1:9)
 
-    @test length(result.items) == 10
-    @test result.period == 11
-    @test all(item.cost > 0.0 for item ∈ result.items)
-    @test all(10 ≤ item.central_leadtime ≤ 20 for item ∈ result.items)
-    @test all(1 ≤ l ≤ 9 for item ∈ result.items for l ∈ item.local_leadtimes)
-    @test all(0 < length(item.orders) ≤ 10 for item ∈ result.items)
-    @test all(n > 0 for item ∈ result.items for unitorders ∈ item.orders for (d, n) ∈ unitorders)
-    @test all(d ∈ 1:11 for item ∈ result.items for unitorders ∈ item.orders for (d, n) ∈ unitorders)
-    @test (validate(result); true)
+        @test length(result.items) == 10
+        @test result.period == 11
+        @test all(item.cost > 0.0 for item ∈ result.items)
+        @test all(10 ≤ item.central_leadtime ≤ 20 for item ∈ result.items)
+        @test all(1 ≤ l ≤ 9 for item ∈ result.items for l ∈ item.local_leadtimes)
+        @test all(0 < length(item.orders) ≤ 10 for item ∈ result.items)
+        @test all(n > 0 for item ∈ result.items for unitorders ∈ item.orders for (d, n) ∈ unitorders)
+        @test all(d ∈ 1:11 for item ∈ result.items for unitorders ∈ item.orders for (d, n) ∈ unitorders)
+        @test (validate(result); true)
+    end
+    @testset "No orders for some" begin
+        result = @test_warn "No orders" (@inferred generateinstance(20, 2, 11, 0.1, 0.5, 0.0, 20.0, 0.5, 0.0, 10:20, 1:9))
+
+        @test length(result.items) < 50
+        @test result.period == 11
+        @test all(item.cost > 0.0 for item ∈ result.items)
+        @test all(10 ≤ item.central_leadtime ≤ 20 for item ∈ result.items)
+        @test all(1 ≤ l ≤ 9 for item ∈ result.items for l ∈ item.local_leadtimes)
+        @test all(0 < length(item.orders) ≤ 2 for item ∈ result.items)
+        @test all(n > 0 for item ∈ result.items for unitorders ∈ item.orders for (d, n) ∈ unitorders)
+        @test all(d ∈ 1:11 for item ∈ result.items for unitorders ∈ item.orders for (d, n) ∈ unitorders)
+        @test (validate(result); true)
+    end
 end
